2f8398f9581e6304c80d6847073b30fd5ecd80ed
Terry Chen <terryschen@gmail.com>
2016-03-22 21:00:43 -0400

    add mini test framework

--- src/tsc.h
+++ src/tsc.h
@@ -1,3 +1,41 @@
+#ifdef NEVER
+  case $0 in
+  *.h)  ;;
+  bash) echo 'Use "bash *.h" and not "bash < *.h"' >&2; exit 1;;
+  sh)   echo 'Use "sh *.h" and not "sh < *.h"' >&2; exit 1;;
+  *)    echo 'Filename must end in ".h"' >&2; exit 1;;
+  esac
+  
+  build=0; run=0; clean=0;
+  case $1 in
+  "")     build=1 ; run=1;;
+  build)  build=1;;
+  run)    run=1;;
+  clean)  clean=1;;
+  *)      echo 'Unrecognized command' >&2; exit 1;;
+  esac
+  
+  if [ "$build" -ne "0" ] ; then
+    echo Compiling $0:
+    echo ${CC=cc} -std=gnu11 -DTSC_DEFINE -DTSC_MAIN -x c ${0} -o ${0%.*}
+    ${CC=cc} -std=gnu11 -DTSC_DEFINE -DTSC_MAIN -x c ${0} -o ${0%.*}
+    chmod +x ${0%.*}
+  fi
+  
+  if [ "$run" -ne "0" ]; then
+    echo 
+    echo output:
+    ./${0%.*}
+    rm -f ./${0%.*}
+  fi
+  
+  if [ "$clean" -ne "0" ]; then
+    rm -f ${0%.*}
+  fi
+  
+  exit 0
+#endif
+
 // tsc.h 
 // Public Domain -- no warranty is offered or implied (see http://www.wtfpl.net/)
 
@@ -16,19 +54,8 @@
 
 #define _POSIX_SOURCE
 #include <stdio.h>
-#include <unistd.h>
-#include <stdint.h>
-#include <termios.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
-#include <inttypes.h>
-#include <limits.h>
-#include <errno.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <dirent.h>
-#include <assert.h>
 
 #ifdef __cplusplus
   #define TSC_EXTERN extern "C"
@@ -201,11 +228,6 @@
     }                                                                                   \
   } while(0)
 
-tsc_vec_define(vi, int)
-typedef tsc_vec_type(vi) vec_int_t;
-tsc_vec_define(vsz, size_t)
-typedef tsc_vec_type(vsz) vec_sz_t;
-
 //}
 
 /// Useful macros
@@ -214,6 +236,27 @@ typedef tsc_vec_type(vsz) vec_sz_t;
 
 #define auto_cstr __attribute__((cleanup(auto_cleanup_cstr))) char *
 #define auto_file __attribute__((cleanup(auto_cleanup_file))) FILE *
+
+#define TSCT_SUITE(name)                void name(void)
+
+#define TSCT_FUNC(name)                 static void name(void)
+#define TSCT_REG(name)                  tsc_test_add_test(name, #name, __func__)
+#define TSCT_CASE(name)                 auto void name(void); TSCT_REG(name); void name(void)
+
+#define TSCT_ASSERT(expr)               tsc_test_assert_run((int)(expr), #expr, __func__, __FILE__, __LINE__)
+#define TSCT_ASSERT_STR_EQ(fst, snd)    tsc_test_assert_run(\
+  strcmp(fst, snd) == 0,                \
+  "strcmp( " #fst ", " #snd " ) == 0",  \
+  __func__, __FILE__, __LINE__)
+
+#define TSCT_ADD_SUITE(name)            tsc_test_add_suite(name)
+#define TSCT_ADD_TEST(func,name,suite)  tsc_test_add_test(func,name,suite)
+#define TSCT_RUN_ALL()                  tsc_test_run_all()
+
+TSC_EXTERN void   tsc_test_assert_run(int result, const char* expr, const char* func, const char* file, int line);
+TSC_EXTERN void   tsc_test_add_test(void (*func)(void), const char* name, const char* suite);
+TSC_EXTERN void   tsc_test_add_suite(void (*func)(void));
+TSC_EXTERN int    tsc_test_run_all(void);
 
 /// helper that never return error
 
@@ -297,9 +340,32 @@ static inline void auto_cleanup_cstr(char **s) {
   }
 }
 
-#define TSC_DEFINE
+//~ #define TSC_DEFINE
 #ifdef TSC_DEFINE
 //}
+
+#include <unistd.h>
+#include <stdint.h>
+#include <termios.h>
+#include <ctype.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <assert.h>
+#include <signal.h>
+#include <time.h>
+
+tsc_vec_define(vi, int)
+typedef tsc_vec_type(vi) vec_int_t;
+tsc_vec_define(vsz, size_t)
+typedef tsc_vec_type(vsz) vec_sz_t;
+
+///////////////////////////
+// String Functions      //
+//////////////////////////{
 
 int tsc_strstartswith(const char *s, const char *start) {
   for(;; s++, start++)
@@ -451,7 +517,7 @@ inline const char * tsc_strlower(char **ret, char *s) {
   return NULL;
 }
 
-///////////////////////////
+//}////////////////////////
 // IO Handling Functions //
 //////////////////////////{
 
@@ -2109,9 +2175,9 @@ const char* tsc_alloc3d_irregular(void ****ret, size_t z, size_t y, size_t ** x_
   return NULL;
 }
 
-//}/////////////////
-// Base64 Enc/Dec //
-///////////////////{
+//}////////////////////////
+// Base64 Enc/Dec        //
+//////////////////////////{
 
 const char* tsc_base64_enc(char **ret, char *data, size_t sz) {
   static char base64_enc_table[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
@@ -2204,9 +2270,344 @@ const char* tsc_base64_dec(char **ret, size_t* retsz, char *data, size_t datsz) 
   
   return NULL;
 }
-//}
+
+//}////////////////////////
+// Test System Function  //
+//////////////////////////{
+
+#define TSCT_MAX_NAME  512
+#define TSCT_MAX_ERROR 2048
+#define TSCT_MAX_TESTS 2048
+
+enum {
+  BLACK         = 0,
+  BLUE          = 1,
+  GREEN         = 2,
+  AQUA          = 3,
+  RED           = 4,
+  PURPLE        = 5,
+  YELLOW        = 6,
+  WHITE         = 7,
+  GRAY          = 8,
+  LIGHT_BLUE    = 9,
+  LIGHT_GREEN   = 10,
+  LIGHT_AQUA    = 11,
+  LIGHT_RED     = 12,
+  LIGHT_PURPLE  = 13,
+  LIGHT_YELLOW  = 14,
+  LIGHT_WHITE   = 15,
+  DEFAULT       = 16,
+};
+
+static const char* tsc_test_colors[] = {
+  "\x1B[0m",
+  "\x1B[34m",
+  "\x1B[32m",
+  "\x1B[36m",
+  "\x1B[31m",
+  "\x1B[35m",
+  "\x1B[33m",
+  "\x1B[37m",
+  "",
+  "\x1B[34m",
+  "\x1B[32m",
+  "\x1B[36m",
+  "\x1B[31m",
+  "\x1B[35m",
+  "\x1B[33m",
+  "\x1B[37m",
+  "\x1B[39m",
+};
+
+typedef struct {
+  void (*func)(void);
+  char name[TSCT_MAX_NAME];
+  char suite[TSCT_MAX_NAME];
+} tsc_test_t;
+
+typedef struct {
+  int test_passing;
+  int suite_passing;
+  int num_asserts;
+  int num_assert_passes;
+  int num_assert_fails;
+  int num_suites;
+  int num_suites_passes;
+  int num_suites_fails;  
+  int num_tests;
+  int num_tests_passes;
+  int num_tests_fails;
+  int assert_err_num;
+  char assert_err_buff[TSCT_MAX_ERROR];
+  char assert_err[TSCT_MAX_ERROR];
+} tsc_test_info_t;
+
+static tsc_test_t       tests[TSCT_MAX_TESTS]  = {0};
+static tsc_test_info_t  test_info         = {0};
+
+static void tsc_test_color(int color) {  
+  printf("%s", tsc_test_colors[color]);
+}
+
+void tsc_test_assert_run(int result, const char* expr, const char* func, const char* file, int line) {
+  test_info.num_asserts++;
+  test_info.test_passing = test_info.test_passing && result;
+  
+  if (result) {
+    test_info.num_assert_passes++;
+  } else {
+    sprintf(test_info.assert_err_buff, 
+      "        %i. Assert [ %s ] (%s:%i)\n", 
+      test_info.assert_err_num+1, expr, file, line );
+    strcat(test_info.assert_err, test_info.assert_err_buff);
+    test_info.assert_err_num++;
+    test_info.num_assert_fails++;
+  }
+}
+
+static void tsc_test_signal(int sig) {
+  test_info.test_passing = 0;
+  switch( sig ) {
+    case SIGFPE:  sprintf(test_info.assert_err_buff,
+      "        %i. Division by Zero\n", test_info.assert_err_num+1);
+    break;
+    case SIGILL:  sprintf(test_info.assert_err_buff,
+      "        %i. Illegal Instruction\n", test_info.assert_err_num+1);
+    break;
+    case SIGSEGV: sprintf(test_info.assert_err_buff,
+      "        %i. Segmentation Fault\n", test_info.assert_err_num+1);
+    break;
+  }
+  
+  test_info.assert_err_num++;
+  strcat(test_info.assert_err, test_info.assert_err_buff);
+  
+  tsc_test_color(RED); 
+  printf("Failed! \n\n%s\n", test_info.assert_err);
+  tsc_test_color(DEFAULT);
+  
+  puts("    | Stopping Execution.");
+  fflush(stdout);
+  exit(0);
+}
+
+static void tsc_test_title_case(char* output, const char* input) {
+  int space = 1;
+  strcpy(output, input);
+  for(unsigned int i = 0; i < strlen(output); i++) {
+    if (output[i] == '_' || output[i] == ' ') {
+      space = 1;
+      output[i] = ' ';
+      continue;
+    } 
+    
+    if (space && output[i] >= 'a' && output[i] <= 'z') {
+      space = 0;
+      output[i] = output[i] - 32;
+      continue;
+    }
+    
+    space = 0;   
+  }
+}
+
+void tsc_test_add_test(void (*func)(void), const char* name, const char* suite) {
+  if (test_info.num_tests == TSCT_MAX_TESTS) {
+    printf("ERROR: Exceeded maximum test count of %i!\n", 
+      TSCT_MAX_TESTS); abort();
+  }
+  
+  if (strlen(name) >= TSCT_MAX_NAME) {
+    printf("ERROR: Test name '%s' too long (Maximum is %i characters)\n", 
+      name, TSCT_MAX_NAME); abort();
+  }
+  
+  if (strlen(suite) >= TSCT_MAX_NAME) {
+    printf("ERROR: Test suite '%s' too long (Maximum is %i characters)\n", 
+      suite, TSCT_MAX_NAME); abort();
+  }
+
+  tsc_test_t test;  
+  test.func = func;
+  tsc_test_title_case(test.name, name);
+  tsc_test_title_case(test.suite, suite);
+  
+  tests[test_info.num_tests++] = test;
+}
+
+void tsc_test_add_suite(void (*func)(void)) {
+  test_info.num_suites++;
+  func();
+}
+
+int tsc_test_run_all(void) {
+  static char current_suite[TSCT_MAX_NAME];
+  
+  signal(SIGFPE,  tsc_test_signal);
+  signal(SIGILL,  tsc_test_signal);
+  signal(SIGSEGV, tsc_test_signal);
+  
+  clock_t start = clock();
+  strcpy(current_suite, "");
+  
+  for(unsigned int i = 0; i < test_info.num_tests; i++) {
+    tsc_test_t test = tests[i];
+    
+    /* Check for transition to a new suite */
+    if (strcmp(test.suite, current_suite)) {
+
+      /* Don't increment any counter for first entrance */
+      if (strcmp(current_suite, "")) {
+        if (test_info.suite_passing) {
+          test_info.num_suites_passes++;
+        } else {
+          test_info.num_suites_fails++;
+        }
+      }
+    
+      test_info.suite_passing = 1;
+      strcpy(current_suite, test.suite);
+      printf("\n\n  ===== %s =====\n\n", current_suite);
+    }
+    
+    /* Run Test */
+    
+    test_info.test_passing = 1;
+    strcpy(test_info.assert_err, "");
+    strcpy(test_info.assert_err_buff, "");
+    test_info.assert_err_num = 0;
+    printf("    | %s ... ", test.name);
+    fflush(stdout);
+    
+    test.func();
+    
+    test_info.suite_passing = test_info.suite_passing && test_info.test_passing;
+    
+    if (test_info.test_passing) {
+      test_info.num_tests_passes++;
+      tsc_test_color(GREEN);
+      puts("Passed!");
+      tsc_test_color(DEFAULT);
+    } else {
+      test_info.num_tests_fails++;
+      tsc_test_color(RED); 
+      printf("Failed! \n\n%s\n", test_info.assert_err);
+      tsc_test_color(DEFAULT);
+    }
+    
+  }
+  
+  if (test_info.suite_passing) {
+    test_info.num_suites_passes++;
+  } else {
+    test_info.num_suites_fails++;
+  }
+  
+  clock_t end = clock();
+  
+  puts("");
+  puts("  +---------------------------------------------------+");
+  puts("  |                      Summary                      |");
+  puts("  +---------++------------+-------------+-------------+");
+  
+  printf("  | Suites  ||");
+  tsc_test_color(YELLOW);  printf(" Total %4d ",  test_info.num_suites);        
+  tsc_test_color(DEFAULT); putchar('|');
+  tsc_test_color(GREEN);   printf(" Passed %4d ", test_info.num_suites_passes); 
+  tsc_test_color(DEFAULT); putchar('|');
+  tsc_test_color(RED);     printf(" Failed %4d ", test_info.num_suites_fails);  
+  tsc_test_color(DEFAULT); puts("|");
+  
+  printf("  | Tests   ||");
+  tsc_test_color(YELLOW);  printf(" Total %4d ",  test_info.num_tests);         
+  tsc_test_color(DEFAULT); putchar('|');
+  tsc_test_color(GREEN);   printf(" Passed %4d ", test_info.num_tests_passes);  
+  tsc_test_color(DEFAULT); putchar('|');
+  tsc_test_color(RED);     printf(" Failed %4d ", test_info.num_tests_fails);   
+  tsc_test_color(DEFAULT); puts("|");
+  
+  printf("  | Asserts ||");
+  tsc_test_color(YELLOW);  printf(" Total %4d ",  test_info.num_asserts);       
+  tsc_test_color(DEFAULT); putchar('|');
+  tsc_test_color(GREEN);   printf(" Passed %4d ", test_info.num_assert_passes); 
+  tsc_test_color(DEFAULT); putchar('|');
+  tsc_test_color(RED);     printf(" Failed %4d ", test_info.num_assert_fails);  
+  tsc_test_color(DEFAULT); puts("|");
+  
+  puts("  +---------++------------+-------------+-------------+");
+  puts("");
+  
+  double total = (double)(end - start) / CLOCKS_PER_SEC;
+  
+  printf("      Total Running Time: %0.3fs\n\n", total);
+  
+  if (test_info.num_suites_fails > 0) { return 1; } else { return 0; }
+}
+
 
 //{
 #endif
 //{
 #endif
+
+#ifdef TSC_MAIN
+
+void base64_enc_test1(void) {
+  char *enc;
+  char *test1 = "Man is distinguished, not only by his reason, " \
+      "but by this singular passion from other animals, which is a lust of the mind, "  \
+      "that by a perseverance of delight in the continued and indefatigable generation of knowledge, " \
+      "exceeds the short vehemence of any carnal pleasure.";
+  char *res1  = "TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlz"  \
+                "IHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2Yg"  \
+                "dGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGlu"  \
+                "dWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRo"  \
+                "ZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4=";
+  TSCT_ASSERT(tsc_base64_enc(&enc, test1, strlen(test1))==NULL);
+  TSCT_ASSERT(strcmp(enc,res1) == 0);
+}
+
+void base64_dec_test1(void) {
+  size_t sz;
+  char *dec;
+  char *test1 = "Man is distinguished, not only by his reason, " \
+      "but by this singular passion from other animals, which is a lust of the mind, "  \
+      "that by a perseverance of delight in the continued and indefatigable generation of knowledge, " \
+      "exceeds the short vehemence of any carnal pleasure.";
+  char *res1  = "TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlz"  \
+                "IHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2Yg"  \
+                "dGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGlu"  \
+                "dWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRo"  \
+                "ZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4=";
+  
+  TSCT_ASSERT(tsc_base64_dec(&dec, &sz, res1, strlen(res1))==NULL);
+  TSCT_ASSERT(strcmp(dec,test1) == 0);
+}
+
+void base64_dec_invalid_char(void) {
+  size_t sz;
+  char *dec;
+  char *res1  = "TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlz"  \
+                "IHNpbmd1bGFyIHBhc3Npb24gZnJ.bSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2Yg"  \
+                "dGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGlu"  \
+                "dWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRo"  \
+                "ZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4=";
+    
+  TSCT_ASSERT(tsc_base64_dec(&dec, &sz, res1, strlen(res1))!=NULL);
+}
+
+void suite_base64(void) {
+  TSCT_REG(base64_enc_test1);
+  TSCT_REG(base64_dec_test1);
+  TSCT_REG(base64_dec_invalid_char);
+}
+
+int main(int argc, const char ** argv) {
+  (void)argc;
+  (void)argv;
+  TSCT_ADD_SUITE(suite_base64);
+  return TSCT_RUN_ALL();
+}
+
+#endif
+
