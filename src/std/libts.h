7b331f12f64e885449a7190188ba9ec07bcd8d23
Terry Chen <terryschen@gmail.com>
2016-05-02 02:53:27 -0400

    update libts.h

--- src/std/libts.h
+++ src/std/libts.h
@@ -4,8 +4,15 @@
 #include "ts_posix.h"
 #include "sds.h"
 #include "ts_macro.h"
+#include "ts_cleanup.h"
 #include "ts_vec.h"
+#include "ts_general.h"
 #include "ts_string.h"
+#include "ts_mdalloc.h"
+#include "ts_base64.h"
+#include "ts_fileio.h"
+#include "ts_pool.h"
+#include "ts_hpool.h"
 #include "ts_test.h"
 
 #endif
