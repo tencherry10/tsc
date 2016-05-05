14b901e93e858d81166984252849535bb1ffb213
Terry Chen <terryschen@gmail.com>
2016-05-04 21:40:49 -0400

    add rellocarray

--- src/std/ts_general.c
+++ src/std/ts_general.c
@@ -1,9 +1,20 @@
 #include "libts.h"
 
+#define MUL_NO_OVERFLOW ((size_t)1 << (sizeof(size_t) * 4))
+
 void * reallocf(void *ptr, size_t size) {
   void *nptr = realloc(ptr, size);
   // OOM condition
   if (!nptr && ptr && size != 0)
 		free(ptr);
   return nptr;
+}
+
+void * reallocarray(void *ptr, size_t nmemb, size_t size) {
+	if ((nmemb >= MUL_NO_OVERFLOW || size >= MUL_NO_OVERFLOW) &&
+	    nmemb > 0 && SIZE_MAX / nmemb < size) {
+		errno = ENOMEM;
+		return NULL;
+	}
+	return realloc(ptr, size * nmemb);
 }
--- src/std/ts_general.h
+++ src/std/ts_general.h
@@ -2,5 +2,6 @@
 #define TS_GENERAL_H__
 
 TSC_EXTERN void * reallocf(void *ptr, size_t size);
+TSC_EXTERN void * reallocarray(void *optr, size_t nmemb, size_t size);
 
 #endif
